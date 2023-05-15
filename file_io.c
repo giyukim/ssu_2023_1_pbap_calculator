07e7c7d635cf788cf92a68e1e5d729a86948d69f
Giyu Kim <giyu1020@gmail.com>
2023-05-15 19:17:41 +0900

    Update status_store() function

--- file_io.c
+++ file_io.c
@@ -2,15 +2,23 @@
 #define __FILE_IO_C__
 
 #include "common.h"
+#include "int_op.c"
 
 #define MAX_FILENAME_STRING 16
+
+void big_int_tostring(int [BINT_ARR_LEN], char [BINT_ARR_LEN]);               // To String
 
 int fileio_write_new(char [MAX_FILENAME_STRING], char [][MAX_STRING], int);     // 새로운 파일로 출력
 int fileio_write_add(char [MAX_FILENAME_STRING], char [MAX_STRING]);            // 기존 파일에 추가 출력
 int fileio_read(char [MAX_FILENAME_STRING]);                                    // 파일 입력 TODO
 int file_remove(char [MAX_FILENAME_STRING]);                                    // 파일 삭제
 
-int status_store(char [5][BINT_ARR_LEN], char [3][BINT_ARR_LEN]);
+void big_int_tostring(int source[BINT_ARR_LEN], char dest[BINT_ARR_LEN])
+{
+    for(int temp_loop_original = 0; temp_loop_original <= big_int_len(source); temp_loop_original++) dest[temp_loop_original] = (char)(source[temp_loop_original] + '0');
+}
+
+int status_store(int [5][BINT_ARR_LEN], int [3][BINT_ARR_LEN]);
 
 int fileio_write_new(char file_name[MAX_FILENAME_STRING], char text[][MAX_STRING], int text_line_count)
 {
@@ -20,9 +28,7 @@ int fileio_write_new(char file_name[MAX_FILENAME_STRING], char text[][MAX_STRING
         fclose(file);
         return FAIL;
     }
-    for (int loop_temp_array_pointer = 0; loop_temp_array_pointer < text_line_count; loop_temp_array_pointer++) {
-        fprintf(file, "%s\n", text[loop_temp_array_pointer]);
-    }
+    for (int loop_temp_array_pointer = 0; loop_temp_array_pointer < text_line_count; loop_temp_array_pointer++) fprintf(file, "%s\n", text[loop_temp_array_pointer]);
     fclose(file);
     return SUCCESS;
 }
@@ -51,7 +57,7 @@ int file_remove(char file_name[MAX_FILENAME_STRING])
     return remove(file_name) ? FAIL : SUCCESS;
 }
 
-int status_store(char variable[5][BINT_ARR_LEN], char history[3][BINT_ARR_LEN])
+int status_store(int variable[5][BINT_ARR_LEN], int history[3][BINT_ARR_LEN])
 {
     /*
      * VAR A
@@ -64,19 +70,18 @@ int status_store(char variable[5][BINT_ARR_LEN], char history[3][BINT_ARR_LEN])
      * VAR H3
      */
     char file_name[MAX_FILENAME_STRING] = "cal.txt";
-    int is_success = 0;
+    char text[8][MAX_STRING] = {0};
 
-    file_remove(file_name);
-    is_success += fileio_write_add(file_name, variable[0]);
-    is_success += fileio_write_add(file_name, variable[1]);
-    is_success += fileio_write_add(file_name, variable[2]);
-    is_success += fileio_write_add(file_name, variable[3]);
-    is_success += fileio_write_add(file_name, variable[4]);
-    is_success += fileio_write_add(file_name, history[0]);
-    is_success += fileio_write_add(file_name, history[1]);
-    is_success += fileio_write_add(file_name, history[2]);
+    big_int_tostring(variable[0], text[0]);
+    big_int_tostring(variable[1], text[1]);
+    big_int_tostring(variable[2], text[2]);
+    big_int_tostring(variable[3], text[3]);
+    big_int_tostring(variable[4], text[4]);
+    big_int_tostring(history[0], text[5]);
+    big_int_tostring(history[1], text[6]);
+    big_int_tostring(history[2], text[7]);
 
-    return is_success == 8 ? SUCCESS : FAIL;
+    return fileio_write_new(file_name, text, 8) ? SUCCESS : FAIL;
 }
 
 #endif
--- test/test_fileio.c
+++ test/test_fileio.c
@@ -2,17 +2,17 @@
 
 int main()
 {
-    char variable[5][BINT_ARR_LEN] = {
-            "93871738",
-            "19383819",
-            "09813",
-            "1",
-            "0"
+    int variable[5][BINT_ARR_LEN] = {
+            {9, 3, 8, 7, 1, 7, 3, 8, 0},
+            {1, 9, 3, 8, 3, 8, 1, 9, 1},
+            {0, 9, 8, 1, 3, 0},
+            {1, 1},
+            {0}
     };
-    char history[3][BINT_ARR_LEN] = {
-            "108083901811",
-            "9838891",
-            "0",
+    int history[3][BINT_ARR_LEN] = {
+            {1, 0, 8, 0, 8, 3, 9, 0, 1, 8, 1, 1},
+            {9, 8, 3, 8, 8, 9, 1},
+            {0},
     };
 
     printf("%d", status_store(variable, history));
